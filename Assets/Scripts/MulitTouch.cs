27c0e6c8f6b59888868d2dce896e8276de160fac
ALZEE23 <muhamadalfarizial726@gmail.com>
2024-12-27 07:51:03 +0700

    feat: add rotation on object

--- Assets/Scripts/MulitTouch.cs
+++ Assets/Scripts/MulitTouch.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MulitTouch : MonoBehaviour
+public class MultiTouch : MonoBehaviour
 {
     private Dictionary<int, Vector3> touchPositionOffsets = new Dictionary<int, Vector3>();
     private Dictionary<int, Transform> activeTouches = new Dictionary<int, Transform>();
+    private int rotationFinger1 = -1;
+    private int rotationFinger2 = -1;
     private Vector3 lastPos;
     private Quaternion lastRot;
 
@@ -19,19 +21,31 @@ public class MulitTouch : MonoBehaviour
 
     void Update()
     {
-        foreach (Touch touch in Input.touches)
+        if (Input.touchCount > 0)
         {
-            switch (touch.phase)
+            foreach (Touch touch in Input.touches)
             {
-                case TouchPhase.Began:
-                    OnTouchDown(touch);
-                    break;
-                case TouchPhase.Moved:
-                    OnTouchDrag(touch);
-                    break;
-                case TouchPhase.Ended:
-                    OnTouchUp(touch);
-                    break;
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        OnTouchDown(touch);
+                        break;
+                    case TouchPhase.Moved:
+                        OnTouchDrag(touch);
+                        break;
+                    case TouchPhase.Ended:
+                        OnTouchUp(touch);
+                        break;
+                }
+            }
+
+            // Handle two-finger rotation
+            if (rotationFinger1 != -1 && rotationFinger2 != -1)
+            {
+                Touch touch1 = Input.GetTouch(rotationFinger1);
+                Touch touch2 = Input.GetTouch(rotationFinger2);
+
+                OnTwoFingerRotate(touch1, touch2);
             }
         }
     }
@@ -43,12 +57,22 @@ public class MulitTouch : MonoBehaviour
         {
             touchPositionOffsets[touch.fingerId] = gameObject.transform.position - touchWorldPosition;
             activeTouches[touch.fingerId] = transform;
+
+            // Assign fingers for rotation if two fingers touch this object
+            if (rotationFinger1 == -1)
+            {
+                rotationFinger1 = touch.fingerId;
+            }
+            else if (rotationFinger2 == -1)
+            {
+                rotationFinger2 = touch.fingerId;
+            }
         }
     }
 
     void OnTouchDrag(Touch touch)
     {
-        if (activeTouches.ContainsKey(touch.fingerId))
+        if (activeTouches.ContainsKey(touch.fingerId) && touch.fingerId != rotationFinger2)
         {
             transform.position = GetTouchWorldPosition(touch) + touchPositionOffsets[touch.fingerId];
         }
@@ -63,9 +87,35 @@ public class MulitTouch : MonoBehaviour
                 transform.position = lastPos;
                 transform.rotation = lastRot;
             }
+
             activeTouches.Remove(touch.fingerId);
             touchPositionOffsets.Remove(touch.fingerId);
+
+            // Clear rotation fingers if they are lifted
+            if (touch.fingerId == rotationFinger1)
+            {
+                rotationFinger1 = -1;
+            }
+            else if (touch.fingerId == rotationFinger2)
+            {
+                rotationFinger2 = -1;
+            }
         }
+    }
+
+    void OnTwoFingerRotate(Touch touch1, Touch touch2)
+    {
+        // Calculate the rotation angle between the two fingers
+        Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+        Vector2 touch2PrevPos = touch2.position - touch2.deltaPosition;
+
+        float prevAngle = Mathf.Atan2(touch2PrevPos.y - touch1PrevPos.y, touch2PrevPos.x - touch1PrevPos.x) * Mathf.Rad2Deg;
+        float currentAngle = Mathf.Atan2(touch2.position.y - touch1.position.y, touch2.position.x - touch1.position.x) * Mathf.Rad2Deg;
+
+        float deltaAngle = currentAngle - prevAngle;
+
+        // Apply the rotation to the object
+        transform.Rotate(Vector3.forward, deltaAngle);
     }
 
     Vector3 GetTouchWorldPosition(Touch touch)
